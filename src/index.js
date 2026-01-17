83325fec3a60f946e1a38d9d786a0b9e5802b282
lahoucinebathoum <lahoucinebathoum3@gmail.com>
2026-01-17 11:53:52 +0100

    Premier commit après réinitialisation de l'historique

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// Création de la racine React
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Rendu de l'application
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// Mesure des performances
+reportWebVitals();
